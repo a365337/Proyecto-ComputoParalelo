5fcd6a3a51793ea7abae78b111681be270e3f969
a365337 <a365337@uach.mx>
2026-03-05 21:04:50 -0600

    Se cierran conexiones de clientes en el servidor y se devuelve los clientes conectados en ese momento

--- cliente.py
+++ cliente.py
@@ -1,25 +1,53 @@
 import socket
 import threading
+from threading import Lock
 
 HOST = 'localhost'
 PORT = 65432
 cliente = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 cliente.connect((HOST, PORT))
 
-def recibir(conn):
+def recibir(conn, lock):
     while True:
         try:
             data = conn.recv(1024)
             if data:
-                print(f"\nServer: {data.decode('utf-8')}\nIngrese un mensaje: ", end="")
+                print(f"\nServer: {data.decode('utf-8')}\n", end="")
             else:
+                conn.close()
                 break
         except Exception as e:
-            print(e)
+            print(f"Error: {e}")
+            conn.close()
             break
     
-
+o = 0
+lock = Lock()
 while True:
-    data = input("Ingrese un mensaje: ")
-    cliente.send(data.encode("utf-8"))
-    threading.Thread(target=recibir, args=(cliente,)).start()
+
+    lock.acquire()
+    print("Seleccione una opción:\n")
+    print("1) Mandar mensajes\n")
+    print("2) Mostrar conectados\n  ")
+    print("3) Salir\n")
+    lock.release()
+    try:
+        o = int(input("Ingrese una opción: "))
+        #lock.release()
+    except ValueError:
+        print("\nIngrese un número válido")
+        continue
+
+    if o == 1:
+        data = input("Ingrese un mensaje: ")
+        cliente.send(data.encode("utf-8"))
+        threading.Thread(target=recibir, args=(cliente,lock)).start()
+    elif o == 2:
+        data = "clientes"
+        cliente.send(data.encode("utf-8"))
+        threading.Thread(target=recibir, args=(cliente,lock)).start()
+    elif o == 3:
+        break
+    
+cliente.close()
+    
--- server.py
+++ server.py
@@ -1,30 +1,55 @@
 import socket
 import threading
+from threading import Lock
 
 HOST = 'localhost'
 PORT = 65432
 
-def mensaje(conn):
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((HOST, PORT))
+s.listen(1)
+
+addresses = []
+lock = Lock()
+
+def mensaje(m, conn, addr):
+    try:
+        if m == "clientes":
+            lock.acquire()
+            data = str(addresses)
+            conn.send(data.encode("utf-8"))
+            lock.release()
+
+        elif m:
+            lock.acquire()
+            print(f"\nCliente #{addresses.index(addr[1])}: {m}\n")
+            lock.release()
+            
+    except Exception as e:
+        print("Error al recibir el mensaje: ", e)
+
+def manejar_conn(addresses, addr, conn):
     while True:
         try:
             data = conn.recv(1024)
-            if data:
-                print(f"\nCliente #{addr[1]}: {data.decode('utf-8')}\nIngrese un mensaje: ", end="")
-            else:
-                break
-        except Exception as e:
-            print("Error al recibir el mensaje: ", e)
+            if not data:
+                raise Exception("Cliente desconectado")
+            mensaje(data.decode('utf-8'), conn, addr)
+        except:
+            print(f"Se quita al cliente #{addresses.index(addr[1])}")
+            addresses.remove(addr[1])
             break
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST, PORT))
-    s.listen()
-    print("Esperando conexion...")
-    conn, addr = s.accept()
 
-    t1 = threading.Thread(target=mensaje, args=(conn,), daemon=True)
-    t1.start()
-
+def recibir_conexiones():
     while True:
-        data = input("Ingrese un mensaje: ")
-        conn.send(data.encode("utf-8"))
+        conn, addr = s.accept()
+        lock.acquire()
+        addresses.append(addr[1])
+        print(f"Conexion Aceptada: {addr}")
+        lock.release()
+        
+        threading.Thread(target=manejar_conn, args=(addresses, addr, conn)).start()
+        print(f"Hilos vivos: {threading.active_count()}")
+
+recibir_conexiones()
